8afe54ec53b2fd651cf182ed43b2ace94f8fa870
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-10-04 19:45:45 +0300

    Add project files.

---
+++ OSMMap/Program.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using OsmSharp;
+using OsmSharp.Streams;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace OSMMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = new FileInfo(@"turkey-latest.osm.pbf").OpenRead())
+            {
+
+
+            }
+        }
+    }
+}
